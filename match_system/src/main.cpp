084e2a5e2cef15f9484cba4e87a00f81969cb219
YuuMoko <2772337221@qq.com>
2023-08-27 16:17:34 +0800

    match-server version2.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -8,6 +8,11 @@
 #include <thrift/transport/TBufferTransports.h>
 
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <vector>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -16,6 +21,63 @@ using namespace ::apache::thrift::server;
 
 using namespace  ::match_service;
 using namespace std;
+
+
+struct Task
+{
+    User user;
+    string type;
+};
+
+struct MessageQueue
+{
+    queue<Task> q;
+    mutex m;
+    condition_variable cv;
+}message_queue;
+
+
+class Pool
+{
+    public:
+        void save_result(int a,int b)
+        {
+            printf("Match Result: %d %d\n", a, b);
+        }
+
+        void match()
+        {
+            while(users.size() > 1)
+            {
+                auto a = users[0], b = users[1];
+                users.erase(users.begin());
+                users.erase(users.begin());
+
+                save_result(a.id,b.id);
+            }
+        }
+
+        void add(User user)
+        {
+            users.push_back(user);
+        }
+
+        void remove(User user)
+        {
+            for(uint32_t i = 0;i < users.size();i ++)
+                if(users[i].id == user.id)
+                {
+                    users.erase(users.begin() + i);
+                    break;
+                }
+
+        }
+    private:
+        vector<User> users;
+}pool;
+
+
+
 class MatchHandler : virtual public MatchIf {
     public:
         MatchHandler() {
@@ -26,6 +88,10 @@ class MatchHandler : virtual public MatchIf {
             // Your implementation goes here
             printf("add_user\n");
 
+            unique_lock<mutex> lok(message_queue.m);
+            message_queue.q.push({user,"add"});
+            message_queue.cv.notify_all();
+
             return 0;
         }
 
@@ -33,10 +99,39 @@ class MatchHandler : virtual public MatchIf {
             // Your implementation goes here
             printf("remove_user\n");
 
+            unique_lock<mutex> lck(message_queue.m);
+            message_queue.q.push({user, "remove"});
+            message_queue.cv.notify_all();
+
             return 0;
         }
 
+
 };
+
+
+void consume_task()
+{
+    while(true)
+    {
+        unique_lock<mutex> lck(message_queue.m);
+        if (message_queue.q.empty())
+        {
+            message_queue.cv.wait(lck);
+        }
+        else
+        {
+            auto task = message_queue.q.front();
+            message_queue.q.pop();
+            lck.unlock();
+
+            if(task.type == "add") pool.add(task.user);
+            else if (task.type == "remove") pool.remove(task.user);
+
+            pool.match();
+        }
+    }
+}
 
 int main(int argc, char **argv) {
     int port = 9090;
@@ -49,6 +144,11 @@ int main(int argc, char **argv) {
     TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
 
     cout << "Start Match Server" << endl;
+
+
+    thread matching_thread(consume_task);
+
+
 
     server.serve();
     return 0;

0c4079fb5e4d2dc87a06d8a8e5f7b1154dff8ed0
YuuMoko <2772337221@qq.com>
2023-08-28 22:06:16 +0800

    match server:5.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -72,26 +72,45 @@ class Pool
             }
         }
 
+
+        bool check_match(uint32_t i,uint32_t j)
+        {
+            auto a = users[i],b = users[j];
+
+            int dt = abs(a.score - b.score);
+            int a_max_dif = wt[i] * 50;
+            int b_max_dif = wt[j] * 50;
+
+            return dt <= a_max_dif && dt <= b_max_dif;
+        }
+
         void match()
         {
+            for(uint32_t i = 0;i < wt.size();i ++)
+                wt[i] ++; //等待秒数 + 1
             while(users.size() > 1)
             {
-                sort(users.begin(),users.end(),[&](User& a,User& b){
-                        return a.score < b.score;
-                        });
                 bool flag = true;
-                for(uint32_t i = 1;i < users.size();i ++)
-                {
-                    auto a = users[i - 1],b = users[i];
-                    if(b.score - a.score <= 50)
+                    for(uint32_t i = 0;i < users.size();i ++)
                     {
-                        users.erase(users.begin() + i - 1,users.begin() + i + 1);
-                        save_result(a.id,b.id);
-
-                        flag = false;
-                        break;
+                        for(uint32_t j = i + 1; j < users.size();j ++)
+                        {
+                            if(check_match(i,j))
+                            {
+                                auto a = users[i],b = users[j];
+                                users.erase(users.begin() + j);
+                                users.erase(users.begin() + i);
+                                wt.erase(wt.begin() + j);
+                                wt.erase(wt.begin() + i);
+                                save_result(a.id,b.id);
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if(!flag) break;
                     }
-                }
+
+                
                 if(flag) break;
             }
         }
@@ -99,6 +118,7 @@ class Pool
         void add(User user)
         {
             users.push_back(user);
+            wt.push_back(0);
         }
 
         void remove(User user)
@@ -107,12 +127,14 @@ class Pool
                 if(users[i].id == user.id)
                 {
                     users.erase(users.begin() + i);
+                    wt.erase(wt.begin() + i);
                     break;
                 }
 
         }
     private:
         vector<User> users;
+        vector<int> wt; // 等待时间，单位 : s
 }pool;
 
 
@@ -186,8 +208,6 @@ void consume_task()
 
             if(task.type == "add") pool.add(task.user);
             else if (task.type == "remove") pool.remove(task.user);
-
-            pool.match();
         }
     }
 }
